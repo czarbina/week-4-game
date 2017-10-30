e4b48626680b3f17ca1603404e262f2a21988f54
Sabrina Kesler <sabrina@getvictorious.com>
2017-10-29 18:53:08 -0700

    Randomized crystals - sort of- and creating html skeletom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,41 @@
 
 var wins;
 var losses;
-var crystalOne;
-var crystalTwo;
-var crystalThree;
-var crystalFour;
 var crystalSum;
 
 var crystalRandom = function(min, max) {
-  min = Math.ceil(19);
-  max = Math.floor(120);
+  min = Math.ceil(1);
+  max = Math.floor(12);
   return Math.floor(Math.random() * (120 - 19)) + 19; 
 }
 
+var crystalOne = function(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(4);
+  return Math.floor(Math.random() * (4 - 1)) + 1; 
+}
+
+var crystalTwo = function(min, max) {
+	crystalTwo != crystalOne;
+  min = Math.ceil(4);
+  max = Math.floor(7);
+  return Math.floor(Math.random() * (7 - 4)) + 4;  
+}
+
+var crystalThree = function(min, max) {
+  min = Math.ceil(7);
+  max = Math.floor(10);
+  return Math.floor(Math.random() * (10 - 7)) + 7;  
+}
+
+var crystalFour = function(min, max) {
+  min = Math.ceil(10);
+  max = Math.floor(13);
+  return Math.floor(Math.random() * (13 - 10)) + 10;  
+}
+
 console.log(crystalRandom());
+console.log(crystalOne());
+console.log(crystalTwo());
+console.log(crystalThree());
+console.log(crystalFour());
