224b0e1d0a7918d3d8bccedf9820180f1c7eb77b
Sabrina Kesler <sabrina@getvictorious.com>
2017-10-30 19:38:22 -0700

    LOGIC. HOORAAAYYY

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,71 @@
 // Variables
 
-var wins;
-var losses;
-var crystalSum;
-
-var crystalRandom = function(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(12);
-  return Math.floor(Math.random() * (120 - 19)) + 19; 
+var win = 0;
+var losses = 0;
+var sum = 0
+var addToSum = function(x)
+	{ sum=x+sum;
+		if (sum===crystalRandom){
+			console.log("You win!");
+			win++;
+			reset();
+		}
+		else if (sum>crystalRandom){
+			console.log("You lose!");
+			losses++;
+			reset();
+	}
 }
 
-var crystalOne = function(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(4);
-  return Math.floor(Math.random() * (4 - 1)) + 1; 
+var crystalRandom = Math.floor(Math.random() * (120 - 19)) + 19;  
+console.log(crystalRandom);
+
+var crystalOne = Math.floor(Math.random() * (4 - 1)) + 1;
+console.log(crystalOne); 
+
+var crystalTwo = Math.floor(Math.random() * (7 - 4)) + 4;  
+console.log(crystalTwo);
+
+var crystalThree = Math.floor(Math.random() * (10 - 7)) + 7;
+console.log(crystalThree);
+
+var crystalFour = Math.floor(Math.random() * (13 - 10)) + 10;
+console.log(crystalFour);  
+
+function reset() {
+	crystalRandom = Math.floor(Math.random() * (120 - 19)) + 19;  
+console.log(crystalRandom);
+	crystalOne = Math.floor(Math.random() * (4 - 1)) + 1;
+console.log(crystalOne);
+crystalTwo = Math.floor(Math.random() * (7 - 4)) + 4;  
+console.log(crystalTwo);
+	crystalThree = Math.floor(Math.random() * (10 - 7)) + 7;
+console.log(crystalThree);
+	crystalFour = Math.floor(Math.random() * (13 - 10)) + 10;
+console.log(crystalFour);
+	
 }
 
-var crystalTwo = function(min, max) {
-	crystalTwo != crystalOne;
-  min = Math.ceil(4);
-  max = Math.floor(7);
-  return Math.floor(Math.random() * (7 - 4)) + 4;  
-}
+$(document).ready(function() {
 
-var crystalThree = function(min, max) {
-  min = Math.ceil(7);
-  max = Math.floor(10);
-  return Math.floor(Math.random() * (10 - 7)) + 7;  
-}
+	$("#rockOne").on("click", function(event){
+		addToSum(crystalOne);
+		console.log(sum);
+	 });
+	$("#rockTwo").on("click", function(event){
+		addToSum(crystalTwo);
+		console.log(sum);
+	 });
+	$("#rockThree").on("click", function(event){
+		addToSum(crystalThree);
+		console.log(sum);
+	 });
+	$("#rockFour").on("click", function(event){
+		addToSum(crystalFour);
+		console.log(sum);
+	 });
+})
 
-var crystalFour = function(min, max) {
-  min = Math.ceil(10);
-  max = Math.floor(13);
-  return Math.floor(Math.random() * (13 - 10)) + 10;  
-}
 
-console.log(crystalRandom());
-console.log(crystalOne());
-console.log(crystalTwo());
-console.log(crystalThree());
-console.log(crystalFour());
+
+
